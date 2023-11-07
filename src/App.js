134320982f4aacad495d5819955ff8cf94fe11f7
swethasakthivel05 <swethasakthivel2004@gmail.com>
2023-11-07 15:16:48 +0530

    commmit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 function App() {
-  return(
-
-<div>
-  <NavBar></NavBar>
-  <Main></Main>
-  <Footer></Footer>
-  </div>
+  return (
+    <div>
+    <NavBar/>
+    <Main/>
+    <Footer/>
+    </div>
   );
-}
-
+    }
+   
 export default App;
