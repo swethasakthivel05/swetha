8c4652836225da41cb1261d2026e89e118fa6bbd
swethasakthivel05 <swethasakthivel2004@gmail.com>
2023-11-07 15:22:32 +0530

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
       <label for="na">Password: &emsp;</label>
       <input id="na" type="box" placeholder="Password"></input>
       <br></br>
-      <label for="na">Password: &emsp;</label>
+      <label for="na">Submit: &emsp;</label>
       <button>Click</button>
     </div>
   );
