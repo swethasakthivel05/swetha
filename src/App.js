5fad95122ea24f55c587e29cea2d2ec4c42716e2
swethasakthivel05 <swethasakthivel2004@gmail.com>
2023-11-07 11:35:21 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Message } from "./Message";
+import { ListElements } from "./ListElements";
+function App() {
+
+  return (
+ // to access the return with various statement <div> is used.
+  <div>
+    <Message></Message>  
+    <ListElements></ListElements>
+  </div>
+  )
+}
+
+export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,15 @@
+//jsx ->javascript xml
+
+//add two numbers
+
+function Message() {
+    return (
+        <div>
+            <h1>From Message.jsx</h1>
+            <p>Welcome...
+            </p>
+        </div>
+    );
+}
+
+export { Message };
