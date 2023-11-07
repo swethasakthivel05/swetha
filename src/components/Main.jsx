0fc2cd5aba9a81256b4496d399f96b80d26f2364
swethasakthivel05 <swethasakthivel2004@gmail.com>
2023-11-07 15:28:51 +0530

    comm i

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,15 @@ import React from "react";
 const Main = () => {
   return (
     <div>
-      <label for="name">Email Id: &emsp;</label>
-      <input id="name" type="box" placeholder="Email ID"></input>
-      <br></br>
-      <label for="na">Password: &emsp;</label>
-      <input id="na" type="box" placeholder="Password"></input>
-      <br></br>
-      <label for="na">Submit: &emsp;</label>
-      <button>Click</button>
+      <centre>
+        <label for="name">Email Id: &emsp;</label>
+        <input id="name" type="Email" placeholder="Email ID"></input>
+        <br></br>
+        <label for="na">Password: &emsp;</label>
+        <input id="na" type="Password" placeholder="Password"></input>
+        <br></br>
+        <button>SUBMIT</button>
+      </centre>
     </div>
   );
 };
