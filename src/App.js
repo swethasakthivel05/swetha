59f9f5b7426a36779334ae41384715a52bdee7f2
swethasakthivel05 <swethasakthivel2004@gmail.com>
2023-11-08 20:36:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-// import "./App.css";
-// import Button from "./day3/Button";
-// import { Cricket } from "./day3/Cricket";
-// import { Dynamicrendering } from "./day3/Dynamicrendering";
-// function App() {
-//   return (
-//     <div>
-//       <Button color="green" >Green Button</Button>
-//       <Button color="red">Red Button</Button>
-//       <Button color="yellow">Yellow Button</Button>
-//       <Button color="blue">Blue Button</Button>
-//       <Dynamicrendering></Dynamicrendering>
-//       <Cricket></Cricket>
-//     </div>
-//   );
-// }
-
-//  export default App;
-
-// // day 2
+// day 3
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import { Cricket } from "./day3/Cricket";
+import Cricket from "./day3/Cricket";
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
-      <br></br>
-      <Main></Main>
-      <br></br>
-      <Footer></Footer>
+      <Cricket></Cricket>
     </div>
   );
     }
    
 export default App;
+
+// day 2
+// import "./App.css";
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+// import Main from "./components/Main";
+// function App() {
+//   return (
+//     <div>
+//       <NavBar></NavBar>
+//       <br></br>
+//       <Main></Main>
+//       <br></br>
+//       <Footer></Footer>
+//     </div>
+//   );
+//     }
+   
+// export default App;
--- src/day3/Cricket.jsx
+++ src/day3/Cricket.jsx
@@ -1,21 +1,61 @@
 import { useState } from "react";
 
-const Count = () => {
-  const [count, setcount] = useState(0);
-  const [count1, setcount1] = useState(0);
+const Cricket = () => {
+  const [count, setCount] = useState(0);
+  const [counts, setCounts] = useState(0);
   return (
     <div>
       <p>
-        Run : {count} &emsp;
-        <br></br>
-        Wicket : {count1} &emsp;
+        Runs: {count} &emsp; Wickets: {counts}
       </p>
       <button
-        classname="px-4 py-4 bg-red-500"
-        onClick={() => setcount(count + 1)}
+        style={{ backgroundColor: "cyan" }}
+        onClick={() => setCount(count + 1)}
       >
-        1 RUN
+        RUNS:1
       </button>
+      {"   "}
+      <button
+        style={{ backgroundColor: "cyan" }}
+        onClick={() => setCount(count + 2)}
+      >
+        RUNS:2
+      </button>{" "}
+      &emsp;
+      <button
+        style={{ backgroundColor: "cyan" }}
+        onClick={() => setCount(count + 4)}
+      >
+        RUNS:4
+      </button>{" "}
+      &emsp;
+      <button
+        style={{ backgroundColor: "cyan" }}
+        onClick={() => setCount(count + 6)}
+      >
+        RUNS:6
+      </button>{" "}
+      &emsp;
+      <button
+        style={{ backgroundColor: "grey-100" }}
+        onClick={() => setCounts(counts + 1)}
+      >
+        WICKET
+      </button>{" "}
+      &emsp;
+      <button
+        style={{ backgroundColor: "skyblue" }}
+        onClick={() => setCount(0)}
+      >
+        Reset
+      </button>{" "}
+      &emsp;
+      <button style={{ backgroundColor: "pink" }} onClick={() => setCounts(0)}>
+        Reset Wicket
+      </button>{" "}
+      &emsp;
     </div>
   );
 };
+
+export default Cricket;
